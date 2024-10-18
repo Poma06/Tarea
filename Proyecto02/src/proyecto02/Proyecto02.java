0610c5f471412310069fd66b43fa490d7681c71f
Poma06 <patriciopro25@gmail.com>
2024-10-17 20:44:01 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ Proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,7 @@
+package proyecto02;
+public class Proyecto02 {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos básico de Java Versionado a traves de Git Manejado desde GitHub Además se usar GitHubClassroom");
+    }
+    
+}
