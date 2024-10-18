641a21d90faccf748c035b664370a9d275c48e14
Poma06 <patriciopro25@gmail.com>
2024-10-17 20:31:27 -0500

    segundo commit

--- Proyecto01/src/proyecto01/Proyecto01.java
+++ Proyecto01/src/proyecto01/Proyecto01.java
@@ -1,7 +1,7 @@
 package proyecto01;
 public class Proyecto01 {
     public static void main(String[] args) {
-        System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+        System.out.println("Ejemplo de Java Manejado desde GitHub Versionado a través de Git creado por Poma06");
     }
     
 }
